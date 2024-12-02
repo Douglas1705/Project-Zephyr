82afb0d06a77301255eeaa2a656e31139eb1a067
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-12-02 17:00:13 -0300

    feat: Importing image files from S3 buckets
    For testing purposes, S3 buckets were configured to import images in the header.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,10 @@ function Header() {
   return (
     <header>
       <h1>Pagina de cabeçalho</h1>
+      <img
+        src="https://compasschallenge-furniro-images.s3.us-east-2.amazonaws.com/logo-furniro.svg"
+        alt="Image of two lines representing the Ovenmaker" title="Furniro-logo"
+      />
     </header>
   );
 }
