cb01f0d49705400d0d1f4e4b9e798004d80df7e0
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-12-02 16:23:00 -0300

    feat: Created the page routes
    Routes created, where header and footer appear in all. A 404 page was created to locate me in case of an error.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Cart from './pages/cart/Cart';
+import CheckoutPage from './pages/checkout/CheckoutPage';
 import Contact from './pages/contact/Contact';
 import HomePage from './pages/homePage/HomePage';
+import NotFound404 from './pages/404Notfound/NotFound404';
 import ShopPage from './pages/shop/ShopPage';
 import SingleProducts from './pages/singleProduct/SingleProducts';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <HomePage />
-      <ShopPage />
-      <SingleProducts />
-      <Cart />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/single-product" element={<SingleProducts />} />
+        <Route path="*" element={<NotFound404 />} /> {/* Rota 404 */}
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/404Notfound/NotFound404.tsx
@@ -0,0 +1,9 @@
+function NotFound404() {
+  return (
+    <section>
+      <h2>Pagina não encontrada</h2>
+    </section>
+  );
+}
+
+export default NotFound404;
