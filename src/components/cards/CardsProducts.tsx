aaecccf5cd1d35b7187b2e802b23b567bf42d5b4
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-12-03 14:31:12 -0300

    feat: creation of json and cards components

---
+++ src/components/cards/CardsProducts.tsx
@@ -0,0 +1,20 @@
+function CardsProducts() {
+  return (
+    <article>
+        <figure>
+            <img src="" alt="" />
+            <span>desconto</span>
+            <title></title>
+            <p></p>
+            <p>
+                RP {valor}
+            </p>
+            <p>
+                Rp {valor}
+            </p>
+        </figure>
+    </article>
+  )
+}
+
+export default CardsProducts;
