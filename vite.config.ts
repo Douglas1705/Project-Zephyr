f74ed3586caaba478ca4ee83379a6b5c4e11e9a1
Douglas1705 <bertoldi_douglas@hotmail.com>
2025-02-19 15:14:12 -0300

    feat: gh pagesp fail

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Projeto-Zephyr/',
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'development' ? '/' : '/Projeto-Zephyr/',
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: './index.html',
+        },
+      },
+    },
+  };
 });
