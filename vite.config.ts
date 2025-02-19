5dd596863735fe0fe7fb0bcf3369d2b21b33781c
Douglas1705 <bertoldi_douglas@hotmail.com>
2025-02-19 16:27:11 -0300

    feat: netfly

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'development' ? '/' : '/Projeto-Zephyr/',
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: './index.html',
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: '/Projeto-Zephyr',
 });
